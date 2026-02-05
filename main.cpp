efc549cdf7d613a9df85b0e5fb7d5a3826747801
Jacob Shok <jacob.shakeri@sjsu.edu>
2026-02-05 10:39:31 -0800

    added polymorphism check

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <memory>
 #include "Animal.h"
 
 // Main function to test the classes
@@ -16,5 +18,16 @@ int main() {
     // Additional testing
     // TODO: Set attributes using setters and test getters
     // TODO: Test polymorphism if desired
+    std::cout  << "test polymorphism" << std::endl;
+    std::vector<std::unique_ptr<Animal>> animals;
+    animals.push_back(std::unique_ptr<Dog>(new Dog("teddy", 3, "poodle")));
+    animals.push_back(std::unique_ptr<Cat>(new Cat("kitty", 2, "Black")));
+    animals.push_back(std::unique_ptr<Animal>(new Animal("normal animal", 5)));
+
+    for (auto& animal : animals) {
+        std::cout << animal->getName() << std::endl;
+        animal->makeSound();
+    }
+
     return 0;
 }
